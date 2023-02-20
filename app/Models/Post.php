2b06872cd3baa5e4d4c628116914b4969c02c685
lucasvarino <lucasvarino@gmail.com>
2023-02-20 13:29:53 -0300

    Chapter 4, SUPER OPTIMIZATION IN DATABASE

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,68 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public string $title;
+    use HasFactory;
 
-    public string $excerpt;
+    protected $guarded = ['id'];
 
-    public string $date;
-
-    public string $body;
-
-    public string $slug;
-
-    /**
-     * @param string $title
-     * @param string $excerpt
-     * @param string $date
-     * @param string $body
-     * @param string $slug
-     */
-    public function __construct(string $title, string $excerpt, string $date, string $body, string $slug)
+    public function category()
     {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
+        return $this->belongsTo(Category::class);
     }
 
-    public static function all() : Collection
+    public function author()
     {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path('posts')))
-                ->map(fn($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn($document) => new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                ))->sortByDesc('date');
-
-        });
+        return $this->belongsTo(User::class, 'user_id');
     }
 
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-
-        if(!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,6 +9,10 @@
                 </a>
             </h1>
 
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->username }}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>
+            </p>
+
             <div>
                 {{ $post->excerpt }}
             </div>
