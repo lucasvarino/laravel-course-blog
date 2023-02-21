18730bdee4d234ae030296c5d65b3aa464477b3b
lucasvarino <lucasvarino@gmail.com>
2023-02-21 09:46:52 -0300

    Making a Dropdown using alpinejs

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,29 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        "posts" => Post::latest()->with('category', 'author')->get()
+        "posts" => Post::latest()->with('category', 'author')->get(),
+        "categories" => Category::all()
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
-        "post" => $post
+        "post" => $post,
+        "categories" => Category::all()
     ]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-   return view('posts', [
-     'posts' => $category->posts->load(['category', 'author'])
-   ]);
+    return view('posts', [
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        "categories" => Category::all()
+    ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
